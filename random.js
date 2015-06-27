d682a24ee80318acd4064c3aac3b50420325a93b
Roberto Carrillo García <rober_cg@telefonica.net>
2015-06-27 13:21:27 +0200

    version 1

---
+++ random.js
@@ -0,0 +1,8 @@
+var numero = Math.random();
+
+if ( numero < 0.5 ) {
+	console.log("el número " + numnero + "ese MENOR que 0.5\n");
+}
+else {
+	console.log("el número " + numnero + "ese MAYOR que 0.5\n");
+}
